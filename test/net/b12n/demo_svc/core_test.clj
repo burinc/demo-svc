ffdfa16126b9c509cb6512cf0c4a691e40643bfb
Burin Choomnuan <burin.choomnuan@gmail.com>
2021-01-23 22:29:43 -0500

    Add test for filter-[valid/invalid]-lines

--- test/net/b12n/demo_svc/core_test.clj
+++ test/net/b12n/demo_svc/core_test.clj
@@ -5,16 +5,38 @@
    [clojure.test
     :refer [deftest is testing]]
    [net.b12n.demo-svc.core
-    :refer [parse-and-validate
+    :refer [filter-invalid-lines
+            filter-valid-lines
+            parse-and-validate
             parse-record]])
   (:import
    [java.time
     DateTimeException
     LocalDate]))
-#_(deftest parse-date-test
-    (testing "parse-date"
-      (is (thrown? DateTimeException (parse-date "24/01/2014" "MM/dd/yyyy")))
-      (is (= (type (parse-date "12/01/2014" "MM/dd/yyyy"))) LocalDate)))
+
+(deftest filter-valid-lines-test
+  (testing "filter-valid-lines"
+    (let [lines (-> "./resources/data-with-invalid-lines.csv"
+                    slurp
+                    str/split-lines)]
+      (is (= (filter-valid-lines lines :csv)
+             '({:last-name "Henry",   :first-name "Jill", :gender "F", :fav-color "White", :date-of-birth "10/18/1980"}
+               {:last-name "Johnson", :first-name "Josh", :gender "M", :fav-color "Blue",  :date-of-birth "06/18/1990"}
+               {:last-name "Barry",   :first-name "Jane", :gender "F", :fav-color "Pink",  :date-of-birth "07/18/1950"}
+               {:last-name "Smith",   :first-name "John", :gender "M", :fav-color "Red",   :date-of-birth "06/18/2000"}))))))
+
+(deftest filter-invalid-lines-test
+  (testing "filter-invalid-lines"
+    (let [lines (-> "./resources/data-with-invalid-lines.csv"
+                    slurp
+                    str/split-lines)]
+      (is (= (filter-invalid-lines lines :csv)
+             '({:data "Johnson,Josh,M,Blue,06/18/19XX",
+                :file-type :csv,
+                :err "\"06/18/19XX\" - failed: valid-date? in: [4] at: [:date-of-birth] spec: :net.b12n.demo-svc.core/date-of-birth\n"}
+               {:data "Barry,Jane,X,Pink,07/18/1950",
+                :file-type :csv,
+                :err "\"X\" - failed: valid-gender? in: [2] at: [:gender] spec: :net.b12n.demo-svc.core/gender\n"}))))))
 
 (deftest parse-record-test
   (testing "parse-record"
